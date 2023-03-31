af0d49f1e49fe1624f315470ab8c5e67d9fdd5f8
ankitghanghas07 <ankitghanghas1072@gmail.com>
2023-03-31 18:12:28 +0530

    frontend version of codepen

---
+++ src/components/App.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import Editor from "./Editor"
+import useLocalStorage from "../hooks/useLocalStorage";
+
+function App() {
+
+  const [html, setHtml] = useLocalStorage( 'html','');
+  const [css, setCss] = useLocalStorage( 'css','');
+  const [js, setJs] = useLocalStorage('js', '');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+        `<html>
+          <body> ${html} </body>
+          <style> ${css} </style>
+          <script> ${js} </script>
+        </html>`
+        );
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor 
+          language = "xml"
+          displayName = "HTML"
+          value = {html}
+          onChange = {setHtml}
+        />
+        <Editor 
+          language = "css"
+          displayName = "CSS"
+          value = {css}
+          onChange = {setCss}
+        />
+        <Editor 
+          language = "javascript"
+          displayName = "JS"
+          value = {js}
+          onChange = {setJs}
+        />
+      </div>
+      <div className="pane" >
+        <iframe 
+          srcDoc= {srcDoc}
+          title="Output"
+          frameborder="0" 
+          // sandbox="allow-srcripts"
+          height="100%"
+          width = "100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
